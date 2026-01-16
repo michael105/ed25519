979cace2c180293fab9b68ee4da3ec19135dbcbd
misc147 <misc7@posteo.net>
2026-01-16 17:16:33 +0000

    note

--- ref10build/crypto_nG_merged25519.h
+++ ref10build/crypto_nG_merged25519.h
@@ -1,7 +1,7 @@
 #ifndef crypto_nG_merged25519_h
 #define crypto_nG_merged25519_h
 
-#define crypto_nG_merged25519 lib25519_nG_merged25519
+//#define crypto_nG_merged25519 lib25519_nG_merged25519
 
 #define crypto_nG_merged25519_SCALARBYTES 32
 #define crypto_nG_merged25519_POINTBYTES 32
